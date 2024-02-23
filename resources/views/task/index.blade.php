f1b18f42cc2d200231fddd4a953e3f1392f538db
Алексей Колесников <kolalexx@yandex.ru>
2024-02-23 15:51:33 +0300

    fixed error 500 v.4

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -19,37 +19,20 @@
                 @endauth
             </div>
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xxl">
                     <table class="w-full">
                         <thead class="border-b-2 border-solid border-black text-left">
                             <tr>
                                 <th>{{ __('views.task.fields.id') }}</th>
-                                <th>{{ __('views.task.fields.status_id') }}</th>
-                                <th>{{ __('views.task.fields.name') }}</th>
-                                <th>{{ __('views.task.fields.created_by_id') }}</th>
-                                <th>{{ __('views.task.fields.assigned_to_id') }}</th>
-                                <th>{{ __('views.task.fields.created_at') }}</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($tasks as $task)
                                 <tr class="border-b border-dashed text-left">
                                     <td>{{ $task->id }}</td>
-                                    <td>{{ $task->status->name }}</td>
-                                    <td>
-                                        <a class="text-blue-600 hover:text-blue-900"
-                                            href="{{ route('tasks.show', ['task' => $task->id]) }}">
-                                            {{ $task->name }}
-                                        </a>
-                                    </td>
-                                    <td>{{ $task->creator->name }}</td>
-                                    <td>{{ $task->executor->name }}</td>
-                                    <td>{{ $task->formattedDate }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
                     </table>
-                </div>
             </div>
         </div>
     </div>
