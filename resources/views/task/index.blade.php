18602af66689ce0597a06c2659d9903a96be9370
Алексей Колесников <kolalexx@yandex.ru>
2024-02-23 22:40:40 +0300

    fixed error 500 v.7

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -5,5 +5,5 @@
 	    {{ Session::get('errors') }}
     @endif
 
-    <h1 class="mb-5">{{ __('views.task.pages.index.title') }}</h1>
+    <h1 class="mb-5">{{ __('views.status.pages.index.title') }}</h1>
 @endsection
