164deb96d924df492cb933471ec8918030f05263
Алексей Колесников <kolalexx@yandex.ru>
2024-02-23 22:30:30 +0300

    fixed error 500 v.6

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -6,18 +6,4 @@
     @endif
 
     <h1 class="mb-5">{{ __('views.task.pages.index.title') }}</h1>
-
-    <div class="py-6">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="flex justify-between">
-                @auth
-                    <a href="{{ route('tasks.create') }}">
-                        <x-primary-button>
-                            {{ __('views.task.pages.index.new') }}
-                        </x-primary-button>
-                    </a>
-                @endauth
-            </div>
-        </div>
-    </div>
 @endsection
